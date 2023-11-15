8b37bac914d284f820fdd4f0a64f0df1debb5518
Tivotal <121335284+Tivotal@users.noreply.github.com>
2023-11-15 07:46:06 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+/* Created by Tivotal */
+
+$(document).ready(function () {
+  $(".card").on("mouseenter", function (e) {
+    (x = e.pageX - $(this).offset().left),
+      (y = e.pageY - $(this).offset().top),
+      $(this).find("span").css({ top: y, left: x });
+  });
+  $(".card").on("mouseout", function (e) {
+    (x = e.pageX - $(this).offset().left),
+      (y = e.pageY - $(this).offset().top),
+      $(this).find("span").css({ top: y, left: x });
+  });
+});
